282ff0cc9d5c8e3a7a79a6ae7d33faa9000edc91
NoeliaPH <noeliaprietoherrero@gmail.com>
2022-01-20 10:59:09 +0100

    hangman

---
+++ src/components/App.js
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import '../styles/main.scss';
+
+function App() {
+  let numberOfErrorsOld = 0;
+
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const [lastLetter, setLastLetter] = useState('');
+  const [word, setWord] = useState('katakroker');
+
+  const handleClickBtn = (ev) => {
+    ev.preventDefault();
+
+    setNumberOfErrors(numberOfErrors+1);
+    console.log(numberOfErrors);
+  }
+
+  const handleChangeLetter = (ev) => {
+    if(/^[a-zA-Z]$/.test(ev.currentTarget.value)) {
+      setLastLetter(ev.currentTarget.value);
+    }
+  }
+
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split('');
+    return wordLetters.map(wordLetter => {
+      return <li>{wordLetter}</li>;
+    });
+  };
+
+  return (
+    <div className="page">
+      <header>
+        <h1 className="header__title">Juego del ahorcado</h1>
+      </header>
+      <main className="main">
+        <section>
+          <div className="solution">
+            <h2 className="title">Solución:</h2>
+            {renderSolutionLetters}
+            <ul className="letters">
+              <li className="letter">k</li>
+              <li className="letter">a</li>
+              <li className="letter"></li>
+              <li className="letter">a</li>
+              <li className="letter">k</li>
+              <li className="letter">r</li>
+              <li className="letter"></li>
+              <li className="letter">k</li>
+              <li className="letter">e</li>
+              <li className="letter">r</li>
+            </ul>
+          </div>
+          <div className="error">
+            <h2 className="title">Letras falladas:</h2>
+            <ul className="letters">
+              <li className="letter">f</li>
+              <li className="letter">q</li>
+              <li className="letter">h</li>
+              <li className="letter">p</li>
+              <li className="letter">x</li>
+            </ul>
+          </div>
+          <form className="form">
+            <label className="title" htmlFor="last-letter">Escribe una letra:</label>
+            <input
+              autoComplete="off"
+              className="form__input"
+              maxLength="1"
+              type="text"
+              name="last-letter"
+              id="last-letter"
+              defaultValue={lastLetter}
+              onChange={handleChangeLetter}
+            />
+          </form>
+          <button type="button" onClick={handleClickBtn}>Incrementar</button>
+        </section>
+        <section className={`dummy error-${numberOfErrors}`}>
+          <span className="error-13 eye"></span>
+          <span className="error-12 eye"></span>
+          <span className="error-11 line"></span>
+          <span className="error-10 line"></span>
+          <span className="error-9 line"></span>
+          <span className="error-8 line"></span>
+          <span className="error-7 line"></span>
+          <span className="error-6 head"></span>
+          <span className="error-5 line"></span>
+          <span className="error-4 line"></span>
+          <span className="error-3 line"></span>
+          <span className="error-2 line"></span>
+          <span className="error-1 line"></span>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
